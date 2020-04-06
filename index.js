eb4b2c487783066c1a5326f501c56c24a7cf46a1
karmodaniel <daniel.carmo2008@gmail.com>
2020-04-05 23:14:13 -0400

    resposta desafio

--- index.js
+++ index.js
@@ -3,16 +3,17 @@ const express = require('express')
 const app = express()
 const fs = require('fs');
 const sha1 = require('js-sha1')
+const formData = require('form-data')
 
 const url = 'https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=e241462c5ac6569cf07336f3a0dd60c54a955709'
 const alfabeto = ['a','b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm','n','o','p','q','r','s','t','u','v','w','x', 'y', 'z']
- 
+
 
 
 function getChallenge(){
       axios.get(url)
         .then(data => {
-         writeFile(data.data)   
+         writeFile(data.data)
         }).catch(err => {
             return err;
         });
@@ -21,16 +22,16 @@ function getChallenge(){
 function writeFile(challenge) {
    const dir = __dirname + '/answer.json'
     fs.writeFile(dir, JSON.stringify(challenge), err => {
-        console.log( err || 'Arquivo salvo.'); 
+        console.log( err || 'Arquivo salvo.');
      })
-  
-}  
+
+}
 
 function decifrar() {
     fs.readFile(__dirname + '/answer.json','utf-8',(err, data) => {
         challenge = JSON.parse(data)
         const {cifrado} = challenge
-        
+
         let notCifrado = []
         for (let i = 0; i < cifrado.length; i++) {
             let aux = alfabeto.indexOf(cifrado[i])
@@ -39,9 +40,9 @@ function decifrar() {
             }else {
                 notCifrado.push(cifrado[i])
             }
-    
-        } 
-        //decifrar 
+
+        }
+        //decifrar
         decifrado = notCifrado.join('');
         challenge.decifrado = decifrado;
         writeFile(challenge);
@@ -51,25 +52,26 @@ function decifrar() {
         writeFile(challenge);
         console.log(challenge);
     })
-    
+
 }
-
-function sendAnswer() {
-    const config = {
-        headers: {
-            'Content-Type': 'multipart/form-data'
-        }
-    }
-
-   return
-   
-}
-
 app.get('/resposta', (req, res) => {
+    const uri = "https://api.codenation.dev/v1/challenge/dev-ps/submit-solution?token=e241462c5ac6569cf07336f3a0dd60c54a955709";
 
+        const form = new formData();
+        form.append('answer', fs.createReadStream('./answer.json'));
+        let options = form.getHeaders()
+        options.method = 'POST';
+        res.send(form)
+        console.log(options)
+        axios.post(uri, form, { headers: options }).then(data => {
+            console.log(data);
+        }).catch(err => {
+            console.log(err)
+        });
 })
 
 app.listen(3000);
 
+
 //getChallenge()
-decifrar()
+//decifrar()
